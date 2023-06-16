4ab68c833d6d55be5cdc5a8c139d933e9d558b0d
Janarthanan2107 <131163725+Janarthanan2107@users.noreply.github.com>
2023-06-16 22:37:03 +0530

    Adding customized Css and normalized Css

--- script/script.js
+++ script/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+// elements
+const moviePriceEl = document.getElementById('movie-price');
+const seatsEl = document.querySelectorAll('.rows .seat:not(.occupy)');
+const countEl = document.getElementById('count');
+const totalEl = document.getElementById('total');
+
+// funtions
+
+
+// events
+for(let i=0;)
+
+// inital settings
